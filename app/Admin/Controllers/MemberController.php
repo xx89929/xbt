085f3a95982eba08567ed1a0e2d33d70610f8df6
lth-win10 <543060074@qq.com>
2018-01-26 22:28:08 +0800

    admin-up1

--- app/Admin/Controllers/MemberController.php
+++ app/Admin/Controllers/MemberController.php
@@ -82,7 +82,7 @@ class MemberController extends Controller
             $grid->member_info_one()->head_pic('头像')->image('',50,50);
             $grid->column('member_info_one.type','会员类型')->display(function ($type){
                 $memberTye = MemberType::where('id',$type)->select('title','color')->first();
-                
+                return "<span class='label' style='background-color:$memberTye->color;color:white'>$memberTye->title</span>";
             });
             $grid->member_info_one()->phone('手机号');
             $grid->member_info_one()->goods('金额');
