f437e599f5f516a23009c71fc30f9067fd3ca82c
lth-win10 <543060074@qq.com>
2018-02-01 13:44:19 +0800

    test1

--- resources/views/home/page/index/partials/case.blade.php
+++ resources/views/home/page/index/partials/case.blade.php
@@ -12,7 +12,7 @@
                             <img class='lazy' data-original="{{$PicPath.$ca->image}}">
                         </div>
                         <div class="ind-case-des text-center">
-                            <p>{{$ca->name}}</p>
+                            <p>{{strlen($ca->name) > 10 ? mb_substr($ca->name,0,10) : $ca->name }}</p>
                             <span>{{$ca->describe}}</span>
                             <span><i>类型：{{$ca->CaseCateOne->title}}</i></span>
                         </div>
