acf0a57bf600a7174dcfe81a8f1407851fec3c55
lth-win10 <543060074@qq.com>
2018-02-06 17:42:10 +0800

    2

--- app/Http/Controllers/Auth/AliPayController.php
+++ app/Http/Controllers/Auth/AliPayController.php
@@ -31,22 +31,34 @@ class AliPayController extends Controller
 
     public function return(Request $request)
     {
-        $pay = new Pay($this->config);
+        $data = Pay::alipay($this->config)->verify(); // 是的，验签就这么简单！
 
-        return $pay->driver('alipay')->gateway()->verify($request->all());
+        dd(dd($data));
+        // 订单号：$data->out_trade_no
+        // 支付宝交易号：$data->trade_no
+        // 订单总金额：$data->total_amount
     }
 
     public function notify(Request $request)
     {
-        $pay = new Pay($this->config);
+        $alipay = Pay::alipay($this->config);
 
-        if ($pay->driver('alipay')->gateway()->verify($request->all())) {
-            file_put_contents(storage_path('notify.txt'), "收到来自支付宝的异步通知\r\n", FILE_APPEND);
-            file_put_contents(storage_path('notify.txt'), '订单号：' . $request->out_trade_no . "\r\n", FILE_APPEND);
-            file_put_contents(storage_path('notify.txt'), '订单金额：' . $request->total_amount . "\r\n\r\n", FILE_APPEND);
-        } else {
-            file_put_contents(storage_path('notify.txt'), "收到异步通知\r\n", FILE_APPEND);
+        try{
+            $data = $alipay->verify(); // 是的，验签就这么简单！
+
+            // 请自行对 trade_status 进行判断及其它逻辑进行判断，在支付宝的业务通知中，只有交易通知状态为 TRADE_SUCCESS 或 TRADE_FINISHED 时，支付宝才会认定为买家付款成功。
+            // 1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号；
+            // 2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额）；
+            // 3、校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方（有的时候，一个商户可能有多个seller_id/seller_email）；
+            // 4、验证app_id是否为该商户本身。
+            // 5、其它业务逻辑情况
+
+            Log::debug('Alipay notify', $data->all());
+        } catch (Exception $e) {
+            // $e->getMessage();
         }
+
+        return $alipay->success()->send();// laravel 框架中请直接 `return $alipay->success()`
 
         echo "success";
     }
