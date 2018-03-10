bc233d1c70320428a33cc63fd051a31f7561d93a
lth-win10 <543060074@qq.com>
2018-03-10 14:10:15 +0800

    bug 0.2

--- storage/framework/views/64c6aead9d2603dd3617a00c344e4645f1fcb031.php
+++ storage/framework/views/64c6aead9d2603dd3617a00c344e4645f1fcb031.php
@@ -1,19 +1,24 @@
+<form method="post" action="<?php echo e(route('partner.create')); ?>">
+    <?php echo e(csrf_field()); ?>
+
 <div class="par_input pull-left">
     <ul class="list-unstyled">
         <li>
-            <input type="text" placeholder="请问贵姓">
+            <input name="name" type="text" placeholder="请问贵姓">
         </li>
         <li>
-            <input type="text" placeholder="联系方式">
+            <input name="phone" type="text" placeholder="联系方式">
         </li>
+        
+            
+        
         <li>
-            <input type="text" placeholder="加盟意向">
-        </li>
-        <li>
-           <textarea rows="4" placeholder="宝贵建议和意见"></textarea>
+           <textarea name="des"  rows="4" placeholder="加盟意向"></textarea>
         </li>
         <li>
            <button type="submit">申请加盟</button>
         </li>
+
     </ul>
 </div>
+</form>
