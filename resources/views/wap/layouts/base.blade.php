3480baa7ddc115ad8b4b6e3fdc63af368c33d346
lth-win10 <543060074@qq.com>
2018-04-20 12:18:10 +0800

    mobile web

--- resources/views/wap/layouts/base.blade.php
+++ resources/views/wap/layouts/base.blade.php
@@ -15,7 +15,7 @@
     {{--<link rel="stylesheet" href="{{url('frozenui/css/frozen.css')}}">--}}
     <link rel="stylesheet" href="{{url('wap/css/style.css')}}">
     <script>
-        var _hmt = _hmt || []; (function() { var hm = document.createElement("script"); hm.src = "https://hm.baidu.com/hm.js?943fb401aa09429271c41f102f12ce0e"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })(); 
+        var _hmt = _hmt || []; (function() { var hm = document.createElement("script"); hm.src = "https://hm.baidu.com/hm.js?943fb401aa09429271c41f102f12ce0e"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();
     </script>
 </head>
 <body>
