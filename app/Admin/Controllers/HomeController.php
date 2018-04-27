4823b407e5b572dd4a8475c9bc12086116509edd
lth-win10 <543060074@qq.com>
2018-04-28 01:01:32 +0800

    Dashboard

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -43,8 +43,8 @@ class HomeController extends Controller
                 });
 
                 $row->column(4, function (Column $column) {
-                    $moneyCount = Order::where('pay_status',1)->select(DB::raw('SUM(order_money) as total_sales'))->first();
-                    dd($moneyCount->total_sales);
+                    $moneyCount = Order::where('pay_status',1)->sum('order_money');
+                    dd($moneyCount);
                     $infoBox = new InfoBox('交易金额(元)', 'rmb', 'red', '/admin/order/list', number_format($moneyCount,2));
                     $column->append($infoBox);
                 });
