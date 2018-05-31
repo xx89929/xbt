cfe53c28a5fd862be06869cadc7b301e7465fe6d
lth-win10 <543060074@qq.com>
2018-05-31 21:13:34 +0800

    we index 0.4

--- app/Http/Controllers/WeChat/CaseController.php
+++ app/Http/Controllers/WeChat/CaseController.php
@@ -15,8 +15,16 @@ class CaseController extends Controller
         $case['category'] = CaseCategory::orderBy('order')->get();
         $case['case'] = CaseXb::select('name','id','describe','image','category')->get()->each(function($c){
             $c->image = env('APP_URL').'/storage/'.$c->image;
-
         });
         return $case;
+    }
+
+    public function caseInfo(Request $request){
+        if($request->get('id')){
+            $res = CaseXb::Id($request->get('id'))->first();
+            $res->image = env('APP_URL').'/storage/'.$res->image;
+            return $res ? $res : null;
+        }
+
     }
 }
